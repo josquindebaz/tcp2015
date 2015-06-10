d75b4d8e54fa4ba843873cde3cc452269d775914
Josquin Debaz <josquin.debaz@gmail.com>
2015-06-10 14:50:21 +0200

    get institutions 	modified:   auteurs.py

--- auteurs.py
+++ auteurs.py
@@ -1,22 +1,36 @@
 import csv,re
 
+
 auteurs  = []
 liens = []
+institutions = {}
 with open('THATCamp FR - Base.csv', 'rb') as csvfile:
 	b =  csv.reader(csvfile, delimiter=',', quotechar='"')
+	compt = 0
 	for row in b:
-		if len(row) > 5:	
-			a = re.split(';',row[5])
-			if  a != ["Auteurs"]:
+		compt += 1
+		if compt == 1:
+			colonnes = row
+		else :
+			if len(row) > 5:	
+				a = re.split(';',row[5])
+				inst = re.split(';',row[11])
 				a = map(lambda x : re.sub("\s*$","",x), a)
 				a = map(lambda x : re.sub("^\s*","",x), a)
 				for c in a:
 					if c not in auteurs:
 						auteurs.append(c)
+						if len(inst) == 1 and len(a) == 1 and inst[0] != "":
+							institutions[c] = inst[0]	
+						elif len(inst) == len(a) and len(inst) > 1 :
+							for cpt in range(len(a)):
+								institutions[ a[cpt] ] = inst[cpt]
+					
 				if len(a) > 1:
 					a = map(lambda x : auteurs.index(x),a)
 					liens.append(a)
 
+print institutions
 				
 F = open("auteurs.net",'w')
 txt = "*Vertices   %s\n" % len(auteurs)
